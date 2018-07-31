584435a71ff84ed3c95a7da786dc5a5c11783e3c
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-07-31 19:37:32 +0530

    *ngIf

---
+++ srcNgIf/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgifComponent } from './ngif/ngif.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgifComponent
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
