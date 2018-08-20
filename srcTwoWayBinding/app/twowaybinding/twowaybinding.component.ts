747bfac043e5f541ddabb6480c1d678d45e3c2f5
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-08-20 16:21:51 +0530

    Two Way Data Binding
    Two way binding - In this concept we can perform two way binding between view and controller(.ts file and .html file).

---
+++ srcTwoWayBinding/app/twowaybinding/twowaybinding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-twowaybinding',
+  templateUrl: './twowaybinding.component.html',
+  styleUrls: ['./twowaybinding.component.css']
+})
+export class TwowaybindingComponent implements OnInit {
+
+  public username = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
