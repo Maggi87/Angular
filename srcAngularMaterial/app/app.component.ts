21a488021088d2f8328539498a8fa7e2c7f3e55d
Dattaprasad <dattaprasad.ingle@rlard.com>
2018-09-09 20:25:38 +0530

    AngularMaterial
    Material Design is a design language developed in 2014 by Google. Expanding upon the
    "card" motifs that debuted in Google Now, Material Design makes more liberal use of
    grid-based layouts, responsive animations and transitions, padding, and depth effects
    such as lighting and shadows.
    Google announced Material Design on June 25, 2014, at the 2014 Google I/
    O conference.
    Designer explained that, "unlike real paper, our digital material can expand and reform
    intelligently. Material has physical surfaces and edges. Seams and shadows provide
    meaning about what you can touch." Google states that their new design language is
    based on paper and ink but implementation will take place in an advanced manner.
    In Angular we can use the concept material design to create our GUI much more better.
    To use the concept of material design follow below link
    https://material.angular.io/
    
    To use material design approach in Angular we have to follow below steps as
    
    Step 1 - Create new Angular project as
    ng new MaterialDemo
    Step 2- Go to that project folder
    cd MaterialDemo
    Step 3 - Install Material components
    npm install --save @angular/material
    Step 4 - Install Component Development Kit
    npm install --save @angular/cdk
    Step 5 - Install component for animations
    npm install --save @angular/animations
    Step 6 - To add gesture support install hammerjs
    npm install --save hammers
    Step 7 - After above steps this path contains some preinstalled themes of
    Material
    node_modules/@angular/material/prebuilt-themes
    
    To use that theme add import statement in styles.css as
    @import "~@angular/material/prebuilt-themes/indigo-pink.css"
    Step 8 - To use animations in our application we have to import it in
    app.module.ts file as
    import {BrowserAnimationsModule} from '@angular/platform-browser/
    animations';
    Step 9 - Add that module name in imports array in app.module.ts file
    @NgModule({
    declarations: [
    AppComponent
    ],
    imports: [
    BrowserModule,
    AppRoutingModule,
    BrowserAnimationsModule
    ],
    providers: [],
    bootstrap: [AppComponent]
    })
    Step 10 - In material every component is defined in separate module
    due to which we have to import that module in app.module.ts file
    ex.
    import {MatButtonModule} from '@angular/material/button';
    step 11-
    Add that module in imports array
    @NgModule({
    declarations: [
    AppComponent
    ],
    imports: [
    BrowserModule,BrowserAnimationsModule,MatButtonModule
    ],
    providers: [],
    bootstrap: [AppComponent]
    })
    Step 12 - Now we can add material component code in .html file as
    <button mat-stroked-button>Marvellous Infosystem</button>
    
    Now we can add any component which is required in our application.

---
+++ srcAngularMaterial/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators,ReactiveFormsModule} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MyApp';
+
+  
+
+}
